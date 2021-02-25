bb116bd46e9c83d35f0c73bff60e7e717ae10509
Jordy Manner <jordy.manner@milkcreation.fr>
2021-02-25 10:06:55 +0000

    event >> 1.0.x-dev

--- src/EventDispatcher.php
+++ src/EventDispatcher.php
@@ -5,13 +5,15 @@ declare(strict_types=1);
 namespace Pollen\Event;
 
 use BadMethodCallException;
+use Exception;
 use League\Event\EventDispatcher as BaseEventDispatcher;
 use League\Event\PrioritizedListenerRegistry;
-use Pollen\Support\Concerns\ConfigBagTrait;
+use Pollen\Support\Concerns\ConfigBagAwareTrait;
 use Pollen\Support\Concerns\ContainerAwareTrait;
-use Pollen\Support\Concerns\ParamsBagTrait;
+use Pollen\Support\Concerns\ParamsBagAwareTrait;
 use Psr\Container\ContainerInterface as Container;
 use Psr\EventDispatcher\ListenerProviderInterface;
+use RuntimeException;
 use Throwable;
 
 /**
@@ -19,9 +21,15 @@ use Throwable;
  */
 class EventDispatcher implements EventDispatcherInterface
 {
-    use ConfigBagTrait;
+    use ConfigBagAwareTrait;
     use ContainerAwareTrait;
-    use ParamsBagTrait;
+    use ParamsBagAwareTrait;
+
+    /**
+     * Instance principale.
+     * @var static|null
+     */
+    private static $instance;
 
     /**
      * @var BaseEventDispatcher
@@ -47,6 +55,23 @@ class EventDispatcher implements EventDispatcherInterface
 
         $this->listenerProvider = new PrioritizedListenerRegistry();
         $this->delegateDispatcher = new BaseEventDispatcher($this->listenerProvider);
+
+        if (!self::$instance instanceof static) {
+            self::$instance = $this;
+        }
+    }
+
+    /**
+     * Récupération de l'instance principale.
+     *
+     * @return static
+     */
+    public static function getInstance(): EventDispatcherInterface
+    {
+        if (self::$instance instanceof self) {
+            return self::$instance;
+        }
+        throw new RuntimeException(sprintf('Unavailable [%s] instance', __CLASS__));
     }
 
     /**
@@ -56,6 +81,8 @@ class EventDispatcher implements EventDispatcherInterface
     {
         try {
             return $this->delegateDispatcher->{$method}(...$arguments);
+        } catch (Exception $e) {
+            throw $e;
         } catch (Throwable $e) {
             throw new BadMethodCallException(
                 sprintf(
